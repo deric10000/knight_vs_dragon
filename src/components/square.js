2a773d050d66df8b90356263a1c682b83fdd4b2e
deric10000 <dericalbright01@hotmail.com>
2017-12-30 21:40:15 -0500

    removed unnecessary code

--- src/components/square.js
+++ src/components/square.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dragon } from './dragon';
 import { Knight } from './knight';
 
-export const Square = ({square, knightPos, changeBorderColor}) => (
+export const Square = ({square, knightPos}) => (
   <div className="square" id="square">
     { square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null }
     { square.x === 3 && square.y === -3 ? <Dragon /> : null }
