90f7df4ed4a64d20ab9b5a1b1dc7ad4ea33faa1e
deric10000 <dericalbright01@hotmail.com>
2017-12-30 21:36:43 -0500

    fixed errors with highlighted squares in the console

--- src/components/square.js
+++ src/components/square.js
@@ -6,14 +6,14 @@ export const Square = ({square, knightPos, changeBorderColor}) => (
   <div className="square" id="square">
     { square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null }
     { square.x === 3 && square.y === -3 ? <Dragon /> : null }
-    { square.x === knightPos.x + 1 && square.y === knightPos.y ||
-      square.x === knightPos.x && square.y === knightPos.y - 1 ||
-      square.x === knightPos.x - 1 && square.y === knightPos.y ||
-      square.x === knightPos.x && square.y === knightPos.y + 1 ||
-      square.x === knightPos.x + 1 && square.y === knightPos.y + 1 ||
-      square.x === knightPos.x - 1 && square.y === knightPos.y - 1 ||
-      square.x === knightPos.x + 1 && square.y === knightPos.y - 1 ||
-      square.x === knightPos.x - 1 && square.y === knightPos.y + 1
+    { (square.x === knightPos.x + 1 && square.y === knightPos.y) ||
+      (square.x === knightPos.x && square.y === knightPos.y - 1) ||
+      (square.x === knightPos.x - 1 && square.y === knightPos.y) ||
+      (square.x === knightPos.x && square.y === knightPos.y + 1) ||
+      (square.x === knightPos.x + 1 && square.y === knightPos.y + 1) ||
+      (square.x === knightPos.x - 1 && square.y === knightPos.y - 1) ||
+      (square.x === knightPos.x + 1 && square.y === knightPos.y - 1) ||
+      (square.x === knightPos.x - 1 && square.y === knightPos.y + 1)
         ? <div className='availableMove' /> : null }
   </div>
 )
