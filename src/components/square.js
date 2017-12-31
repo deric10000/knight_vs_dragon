8ee6ebfa38511ae32b7bd8f0f9d0457988e1d9b2
deric10000 <dericalbright01@hotmail.com>
2017-12-30 21:33:21 -0500

    highlighted areas surrounding the knight inform user where player can move

--- src/components/square.js
+++ src/components/square.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Dragon } from './dragon';
 import { Knight } from './knight';
 
-export const Square = ({square, knightPos}) => (
+export const Square = ({square, knightPos, changeBorderColor}) => (
   <div className="square" id="square">
     { square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null }
     { square.x === 3 && square.y === -3 ? <Dragon /> : null }
+    { square.x === knightPos.x + 1 && square.y === knightPos.y ||
+      square.x === knightPos.x && square.y === knightPos.y - 1 ||
+      square.x === knightPos.x - 1 && square.y === knightPos.y ||
+      square.x === knightPos.x && square.y === knightPos.y + 1 ||
+      square.x === knightPos.x + 1 && square.y === knightPos.y + 1 ||
+      square.x === knightPos.x - 1 && square.y === knightPos.y - 1 ||
+      square.x === knightPos.x + 1 && square.y === knightPos.y - 1 ||
+      square.x === knightPos.x - 1 && square.y === knightPos.y + 1
+        ? <div className='availableMove' /> : null }
   </div>
 )
