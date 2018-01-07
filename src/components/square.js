9fc00f33ae1eeb0aa788d3e7704a7804e3a05b70
deric10000 <dericalbright01@hotmail.com>
2018-01-06 21:04:29 -0500

    updated movement and fixed css errors with available moves

--- src/components/square.js
+++ src/components/square.js
@@ -4,9 +4,9 @@ import { Knight } from './knight';
 
 export const Square = ({square, knightPos, knightIsActive, lastKnightPos}) => (
   <div className="square" id="square">
-    { square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null }
     { square.x === 3 && square.y === -3 ? <Dragon /> : null }
     { knightIsActive === true ? (
+        (square.x === lastKnightPos.x && square.y === lastKnightPos.y) ||
         (square.x === lastKnightPos.x + 1 && square.y === lastKnightPos.y) ||
         (square.x === lastKnightPos.x && square.y === lastKnightPos.y - 1) ||
         (square.x === lastKnightPos.x - 1 && square.y === lastKnightPos.y) ||
@@ -15,8 +15,10 @@ export const Square = ({square, knightPos, knightIsActive, lastKnightPos}) => (
         (square.x === lastKnightPos.x - 1 && square.y === lastKnightPos.y - 1) ||
         (square.x === lastKnightPos.x + 1 && square.y === lastKnightPos.y - 1) ||
         (square.x === lastKnightPos.x - 1 && square.y === lastKnightPos.y + 1)
-          ? <div className='availableMove' /> : null
-      ) : null
+          ? <div className="availableMove">
+              { square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null }
+            </div> : null
+      ) : square.x === knightPos.x && square.y === knightPos.y ? <Knight /> : null
     }
   </div>
 )
